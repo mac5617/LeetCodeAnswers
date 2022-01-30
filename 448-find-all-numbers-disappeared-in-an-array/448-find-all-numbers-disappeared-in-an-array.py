33b172637d1fa65bb881695891357c46ac65ffd9
Michael Campbell <36902190+mac5617@users.noreply.github.com>
2022-01-30 08:40:47 -0500

    Time: 415 ms (47.08%), Space: 24.4 MB (33.79%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -1,6 +1,14 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        expected = set(range(1,len(nums)+1))
-        fixed = set(nums)
-        res = expected - fixed
-        return list(res)
+        res = []
+        count = len(nums)
+        nums = list(set(nums))
+        nums.sort()
+        pointer = 0
+        for i in range(1,count+1):
+            if pointer<len(nums):
+                if i == nums[pointer]:
+                    pointer+=1
+                    continue
+            res.append(i)
+        return res
