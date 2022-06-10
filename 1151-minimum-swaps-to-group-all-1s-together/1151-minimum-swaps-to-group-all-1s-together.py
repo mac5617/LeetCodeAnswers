4193acd099991b6b6a3e64deea4231f5f9fe659b
Michael Campbell <36902190+mac5617@users.noreply.github.com>
2022-06-10 18:06:47 -0400

    Time: 834 ms (85.72%), Space: 18.1 MB (8.69%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
@@ -1,9 +1,14 @@
 class Solution:
     def minSwaps(self, data: List[int]) -> int:
+        # binary array so there are only ones and zeros. Find all ones in array.
         windowLength = sum(data)
+        # Same concept to find create our sliding window
         windowOnes = sum(data[:windowLength])
+        # return value which is used to compare the optimal window
         maxOnes = windowOnes
+        # loop that goes through the window range
         for i in range(windowLength,len(data)):
+            # add and remove ones according to the value
             windowOnes -= data[i-windowLength]
             windowOnes += data[i]
             maxOnes = max(windowOnes,maxOnes)
